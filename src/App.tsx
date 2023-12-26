6f9a568c8b54ae57e00738375c766b8b2418de58
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-12-26 13:54:12 +0330

    import Slider  & Home componenet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import ResponsiveAppBar from './components/Header'
+import Slider from './components/Slider'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <>
      <ResponsiveAppBar/>
+     <Slider/>
+     <Home/>
     </>
   )
 }
