1c5b477390dd562b7cc6d13544505733e374a5c1
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-12-28 10:49:08 +0330

    update home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
-import '../assets/Home.css' 
+import '../assets/Home.css'
 import image1 from '../assets/image1.png'
 import image2 from '../assets/image2.png'
 import image3 from '../assets/image3.png'
 import image4 from '../assets/image4.png'
-
+import  '@mui/material/colors';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
 const Home = () => {
     return (
@@ -37,7 +39,7 @@ const Home = () => {
                     </section>
 
 
-                    <section className='section-three' style={{ backgroundColor: 'blue' }}>
+                    <section className='section-three'>
 
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="1 2 1000 95" preserveAspectRatio="none">
                             <path className='svg' d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7
@@ -66,57 +68,94 @@ const Home = () => {
                                 <Paper elevation={3}>
                                     <img src={image2} alt="" />
                                     <div>
-                                    <p>دوره ۵۰۴ واژه ضروری در انگلیسی</p>
-                                </div>
-                                <div>
-                                    <p>
-                                        <span>۱,۵۸۰,۰۰۰ </span>تومان
-                                    </p>
-                                </div>
+                                        <p>دوره ۵۰۴ واژه ضروری در انگلیسی</p>
+                                    </div>
+                                    <div>
+                                        <p className='div-p'>
+                                            <span className='bold'>۱,۵۸۰,۰۰۰ </span>تومان
+                                        </p>
+                                    </div>
+                                    <div className='div-a'>
+                                        <a href="">
+                                            <span>اطلاعات بیشتر</span>
+                                            <ChevronLeftIcon/>
+
+                                        </a>
+                                    </div>
                                 </Paper>
                                 <Paper elevation={3}>
                                     <img src={image1} alt="" />
                                     <div>
-                                    <p>دوره آموزش فونتیک پلاس</p>
-                                </div>
-                                <div>
-                                    <p>
-                                        <span>۸۹۰,۰۰۰ </span>تومان
-                                    </p>
-                                </div>
+                                        <p>دوره آموزش فونتیک پلاس</p>
+                                    </div>
+                                    <div>
+                                        <p className='div-p'>
+                                            <span className='bold'>۸۹۰,۰۰۰ </span>تومان
+                                        </p>
+                                    </div>
+                                    <div className='div-a'>
+                                        <a href="">
+                                            <span>اطلاعات بیشتر</span>
+                                            <ChevronLeftIcon/>
+                                            
+                                        </a>
+                                    </div>
                                 </Paper>
-                                
+
                                 <Paper elevation={3}>
                                     <img src={image4} alt="" />
                                     <div>
-                                    <p>دوره جامع فرزند دو زبانه</p>
-                                </div>
-                                <div>
-                                    <p>
-                                        <span>۳۱۵۰,۰۰۰</span>تومان
-                                    </p>
-                                </div>
+                                        <p>دوره جامع فرزند دو زبانه</p>
+                                    </div>
+                                    <div>
+                                        <p className='div-p'>
+                                            <span className='bold'>۳۱۵۰,۰۰۰ </span>تومان
+                                        </p>
+                                    </div>
+                                    <div className='div-a'>
+                                        <a href="">
+                                            <span>اطلاعات بیشتر</span>
+                                            <ChevronLeftIcon/>
+
+                                        </a>
+                                    </div>
                                 </Paper>
-                                
-                                
+
+
                                 <Paper elevation={3}>
                                     <img src={image3} alt="" />
 
-                                <div>
-                                    <p>دوره آموزش استپ آپ</p>
-                                </div>
-                                <div>
-                                    <p>
-                                        <span>۹۰۰,۰۰۰</span>تومان
-                                    </p>
-                                </div>
+                                    <div>
+                                        <p>دوره آموزش استپ آپ</p>
+                                    </div>
+                                    <div>
+                                        <p className='div-p'>
+                                            <span className='bold'>۹۰۰,۰۰۰ </span>تومان
+                                        </p>
+                                    </div>
+                                    <div className='div-a'>
+                                        <a href="">
+                                            <span>اطلاعات بیشتر</span>
+                                            
+                                            <ChevronLeftIcon/>
+                                            
+                                        </a>
+                                    </div>
                                 </Paper>
                             </Box>
+                        </section>
+                        <section className='section-foure'>
+                            <Button variant="contained" disableElevation sx={{color: '#03AEC6',display: 'flex',
+                                    margin: 'auto',
+                                     
+                                    }}>
+                                مشاهده همه دوره‌ها
+                            </Button>
                         </section>
                     </section>
 
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 90" preserveAspectRatio="none">
-                        <path style={{ fill: 'blue' }} className="svg" d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7
+                        <path style={{ fill: '#7C57E6' }} className="svg" d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7
 	c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4
 	c21.2-8.1,52.2-18.2,79.7-24.2C399.3,7.9,411.6,7.5,421.9,6.5z"></path>
                     </svg>
