0d8b6bf8baaf09a5bccdb352f79f6e183ff1085e
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-01-01 09:39:20 +0330

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import logo from '../assets/logo.webp';
 import enamad from '../assets/enamad.png'
+import { Button } from '@mui/material';
 
 const Footer = () => {
 
@@ -22,16 +23,16 @@ const Footer = () => {
                     '& > :not(style)': {
                         m: 2,
                         maxWidth: 300,
-                        height: 300,
+                        height: 250,
                         backgroundColor: '#E6F4FF'
                     },
                 }}
             >
-                <Paper className='paper'>
+                <Paper elevation={0} className='paper'>
                     <h3>درباره آکادمی روان</h3>
                     <p>آکادمی روان یکی از پرتلاش‌ترین و بروزترین وبسایت‌های آموزشی در سطح ایران است که همیشه تلاش کرده تا بتواند بروزترین مقالات و دوره‌های آموزشی را در اختیار علاقه‌مندان به یادگیری زبان انگلیسی قرار دهد.</p>
                 </Paper>
-                <Paper className='paper'>
+                <Paper elevation={0} className='paper'>
                     <h3>دسترسی سریع</h3>
                     <a>مقالات</a>
                     <a>دوره های آموزشی</a>
@@ -39,7 +40,7 @@ const Footer = () => {
                     <a>ارتباط با </a>
                     <a>سیاست حریم خصووصی</a>
                 </Paper>
-                <Paper className='paper'>
+                <Paper elevation={0} className='paper'>
                     <h3>ارتباط باما</h3>
                     <p>پشتیبانی: 36297355-035</p>
                     <p>ثبت‌نام و پشتیبانی دوره‌ها:</p>
@@ -47,16 +48,18 @@ const Footer = () => {
                     <p>ایمیل:</p>
                     <p>academi.karimi@gmail.com</p>
                 </Paper>
-                <Paper className='paper'>
+                <Paper elevation={0} className='paper'>
                     <h3>اطلاع از فروش ویژه</h3>
                     <label htmlFor="">ایمیل خود را وارد کنید!</label>
                     <input type="text" />
+                    <Button variant="contained" disableElevation sx={{ bgcolor: '#11B9CD', height: 50, borderRadius: 2 }}>
+                        به من اطلاع دهید!    
+                    </Button>
                 </Paper>
-                <Paper className='paper'>
+                <Paper elevation={0} className='paper'>
                     <img src={logo} alt="Logo" />
                     <img src={enamad} alt="Enamad" />
                 </Paper>
-
             </Box>
         </section>
     )
