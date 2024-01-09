7b5b25f9ada0cad8e25ca8bb1f4e8b2fcc5d6ff6
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-01-09 11:36:56 +0330

    update app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,12 @@
-import './App.css'
-import ResponsiveAppBar from './components/Header'
-import Slider from './components/Slider'
-import Home from './components/pages/Home'
-import Footer from './components/Footer'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { green, brown } from '@mui/material/colors';
+import Layout from "./layout"
+import Home from "./pages/Home"
 
-
-declare module '@mui/material/styles' {
-  interface PaletteColor {
-    darker?: string;
-  }
-
-  interface SimplePaletteColorOptions {
-    darker?: string;
-  }
-}
-
-function App() {
-
-  
-  const theme = createTheme({
-    palette: {
-      primary: {
-        light: brown[50],
-
-        dark: brown[700],
-        darker: brown[900],
-        main: green[400],
-      },
-    },
-  });
-
+const App = () => {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <ResponsiveAppBar />
-        <Slider />
+      <Layout>
         <Home />
-        <Footer />
-      </ThemeProvider>
+      </Layout>
     </>
   )
 }
