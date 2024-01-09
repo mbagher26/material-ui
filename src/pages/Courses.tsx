dd449f111ea46a72c2c572e3543477c8902664db
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-01-09 19:00:12 +0330

    update import svg

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -8,6 +8,7 @@ import image1 from '../assets/image1.png'
 import image2 from '../assets/image2.png'
 import image3 from '../assets/image3.png'
 import image4 from '../assets/image4.png'
+import Topcourse from '../components/icon/Topcourse';
 
 
 
@@ -31,11 +32,7 @@ const Courses = () => {
     return (
         <>
             <section className='section-three'>
-                <svg className='svg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
-                    <path className="elementor-shape-fill" d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7
-	c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4
-	c21.2-8.1,52.2-18.2,79.7-24.2C399.3,7.9,411.6,7.5,421.9,6.5z"></path>
-                </svg>
+               <Topcourse backgroundColor='#7C57E6' color='#fff'/>
 
 
                 <section className='section-three-one'>
@@ -141,12 +138,7 @@ const Courses = () => {
                     </Button>
                 </section>
             </section>
-
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 90" preserveAspectRatio="none">
-                <path style={{ fill: '#7C57E6' }} className="svg" d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7
-	c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4
-	c21.2-8.1,52.2-18.2,79.7-24.2C399.3,7.9,411.6,7.5,421.9,6.5z"></path>
-            </svg>
+                <Topcourse backgroundColor='#fff' color='#7C57E6'/>
         </>
     )
 }
