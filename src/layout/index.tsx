ba6be68ad5521cc2b3f922ca4c4554a0e1c024d1
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-01-09 11:40:20 +0330

    create layout folder and index.tsx

---
+++ src/layout/index.tsx
@@ -0,0 +1,45 @@
+import './App.css'
+import ResponsiveAppBar from '../components/Header'
+import Slider from '../components/Slider'
+import Footer from '../components/Footer'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { green, brown } from '@mui/material/colors';
+import { FunctionComponent, ReactElement } from 'react';
+
+
+declare module '@mui/material/styles' {
+  interface PaletteColor {
+    darker?: string;
+  }
+
+  interface SimplePaletteColorOptions {
+    darker?: string;
+  }
+}
+
+function Layout({children}:{children:ReactElement}) {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: brown[50],
+
+        dark: brown[700],
+        darker: brown[900],
+        main: green[400],
+      },
+    },
+  });
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <ResponsiveAppBar />
+        <Slider />
+            {children}
+        <Footer />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default Layout
