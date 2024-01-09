1503769009933f90ef95bda7ec0fb1ce35710f97
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-01-09 10:49:17 +0330

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,43 @@ import ResponsiveAppBar from './components/Header'
 import Slider from './components/Slider'
 import Home from './components/pages/Home'
 import Footer from './components/Footer'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { green, brown } from '@mui/material/colors';
+
+
+declare module '@mui/material/styles' {
+  interface PaletteColor {
+    darker?: string;
+  }
+
+  interface SimplePaletteColorOptions {
+    darker?: string;
+  }
+}
 
 function App() {
 
+  
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: brown[50],
+
+        dark: brown[700],
+        darker: brown[900],
+        main: green[400],
+      },
+    },
+  });
+
   return (
     <>
-     <ResponsiveAppBar/>
-     <Slider/>
-     <Home/>
-     <Footer/>
+      <ThemeProvider theme={theme}>
+        <ResponsiveAppBar />
+        <Slider />
+        <Home />
+        <Footer />
+      </ThemeProvider>
     </>
   )
 }
