5a350025a99122ee8911a01d720f303f7d8bb155
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-01-16 13:59:01 +0330

    update import Topcourse

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -8,7 +8,7 @@ import image1 from '../assets/image1.png'
 import image2 from '../assets/image2.png'
 import image3 from '../assets/image3.png'
 import image4 from '../assets/image4.png'
-import Topcourse from '../components/icon/Topcourse';
+import Topcourse from '../assets/svg/Topcourse';
 
 
 
