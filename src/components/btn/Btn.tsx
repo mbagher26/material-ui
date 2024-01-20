6b0c2776861efccffaa6458b11b4fc0131174d50
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-01-20 12:17:58 +0330

    add css with makestyle

--- src/components/btn/Btn.tsx
+++ src/components/btn/Btn.tsx
@@ -1,20 +1,41 @@
 import Button from '@mui/material/Button';
 import IBtn from './Btntypes';
 import CircularProgress from '@mui/material/CircularProgress';
+import { makeStyles } from '@mui/styles';
 
-export default function Btn(props:IBtn) {
+
+const useStyles = makeStyles({
+  btn: {
+      display: 'flex !important',
+      margin: '2px !important',
+      marginRight: '10px !important',
+      justifyContent: 'center !important',
+      marginBottom: '15px !important',
+      marginTop: '15px !important',
+      backgroundColor: '#11B9CD !important',
+      minWidth:200,
+      maxWidth:250,
+      height: 50
+  },
+
+});
+
+const Btn = (props:IBtn) => {
+  const classes = useStyles();
+
   const {children, variant, isloading, title} = props;
+  
   return (
-    <Button variant="contained">
+    <Button variant={variant} className={classes.btn}>
       {
         isloading ? 
         (
           <CircularProgress />
         ):(
-
           children ? children : title
         )
       }
     </Button>
   );
 }
+export default Btn
