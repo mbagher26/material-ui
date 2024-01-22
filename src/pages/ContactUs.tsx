07b369de439b92ebc9a229bec38ac7594a257fd8
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-01-22 11:17:12 +0330

    update input in contactUs

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,26 +1,13 @@
-import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
+
 import Box from '@mui/material/Box';
-import { makeStyles } from '@mui/styles';
 import Btn from '../components/btn/Btn';
-
-
-const useStyles = makeStyles({
-
-    input_label: {
-        color: 'white !important',
-        transformOrigin: 'right !important',
-        
-    },
-    textfield: {
-        backgroundColor: 'white !important',
-        borderRadius: 5
-    }
-});
+import  Input  from '../components/input/Input';
+import PersonIcon from '@mui/icons-material/Person';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 
 const ContactUs = () => {
 
-    const classes = useStyles();
+    
 
     return (
         <>
@@ -28,25 +15,10 @@ const ContactUs = () => {
                 <div className='div-contact'>
 
                     <p>دریافت مشاوره رایگان برای شروع آموزش زبان انگلیسی فرم زیر را تکمیل کنید:</p>
-                    <Box
-                     
-                    >
-                        <InputLabel shrink htmlFor="نام و نام خانوادگی" className={classes.input_label}>
-                            نام و نام خانوادگی:
-                        </InputLabel>
-                        <TextField
-                            className={classes.textfield}
-                            id="demo-helper-text-aligned"
-                        />
-                        <InputLabel shrink htmlFor="شماره تماس" className={classes.input_label}>
-                            شماره تماس:
-                        </InputLabel>
-                        <TextField
-                            className={classes.textfield}
-                            id="demo-helper-text-aligned-no-helper"
-                        />
+                    <Box>
+                        <Input label='نام و نام خانوادگی:' icon={<PersonIcon/>}/>
+                        <Input label='شماره تماس:' icon={<PhoneIphoneIcon/>}/>
                         <Btn variant='contained' title='ثبت درخواست مشاوره'/>
-                       
                     </Box>
                 </div>
             </section>
